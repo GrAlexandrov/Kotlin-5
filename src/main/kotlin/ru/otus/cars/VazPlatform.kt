ba41c70bcaf0742206ab791a03093f19d002854e
GrAlexandrov <alexandrov.18rus@gmail.com>
2024-11-24 23:28:47 +0400

    Домашнее задание Kotlin #5 финал

--- src/main/kotlin/ru/otus/cars/VazPlatform.kt
+++ src/main/kotlin/ru/otus/cars/VazPlatform.kt
@@ -15,6 +15,7 @@ abstract class VazPlatform(override val color: String) : Car {
     // Абстрактное свойство двигателя
     abstract val engine: VazEngine
     override var typeOfFuel: String = ""
+    override var fuelLevel: Int = 10
 }
 
 // Перечисление двигателей ВАЗ
